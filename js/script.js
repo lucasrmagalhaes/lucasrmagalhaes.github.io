deb4598f61db11737e7efa352120c1735279e670
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-12-27 12:55:51 -0300

    scroll-up button

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
         } else {
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({scrollTop: 0});
     });
 
     // toggle menu/navbar script
